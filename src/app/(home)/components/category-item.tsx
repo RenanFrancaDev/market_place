10694f765b1038b0f6d688f3c2ad5cfe9db4fa65
Renan Franca <renanfranca.dev@gmail.com>
2023-10-21 12:23:22 -0300

    feat: add link on category-item in home page

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -3,26 +3,22 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
-import { HeadphonesIcon, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
+import Link from "next/link";
+
 
 interface CategoryItemProps{
     category: Category
 }
 
 const CategoryItem = ({category}: CategoryItemProps) => {
-    const categoryIcon = {
-        keyboards: <KeyboardIcon size={16}/>,
-        monitors: <MonitorIcon size={16}/>,
-        headphones: <HeadphonesIcon size={16}/>,
-        mousepads: <SquareIcon size={16}/>,
-        speakers: <SpeakerIcon size={16}/>,
-        mouses: <MouseIcon size={16}/>
-    }
+   
     return ( 
+       <Link href={`/category/${category.slug}`}>
         <Badge variant="outline" className="flex items-center justify-center py-3 gap-3 rounded-lg">
             {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
             <span className="font-bold text-xs ">{category.name}</span>
         </Badge>
+       </Link>
      );
 }
  
