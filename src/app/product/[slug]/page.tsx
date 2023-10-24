7f4a0ce4c074c0b6a9929573dd73b277e2b74372
Renan Franca <renanfranca.dev@gmail.com>
2023-10-24 19:05:10 -0300

    feat: add product list of the same category in product page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,31 +2,45 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
 import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/products";
+import ProductList from "@/components/ui/product-list";
 
-interface ProducDetailsPageProps{
-    params:{
-        slug: string
-    }
+interface ProducDetailsPageProps {
+  params: {
+    slug: string;
+  };
 }
- 
-const ProductDetailPage = async ({params: { slug }}: ProducDetailsPageProps) => {
-    const product = await prismaClient.product.findFirst({
-        where: {
-            slug: slug,
-        }
-    })
 
-    if (!product) return null
+const ProductDetailPage = async ({
+  params: { slug },
+}: ProducDetailsPageProps) => {
+  const product = await prismaClient.product.findFirst({
+    where: {
+      slug: slug,
+    },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 
-    return ( 
-        <div>
-            <ProductImages
-            name={product.name}
-            imageUrls={product.imageUrls}/>
-            <ProductInfo
-            product={computeProductTotalPrice(product)}/>
-        </div>
-     );
-}
- 
+  if (!product) return null;
+
+  return (
+    <div>
+      <ProductImages name={product.name} imageUrls={product.imageUrls} />
+      <ProductInfo product={computeProductTotalPrice(product)} />
+      <ProductList products={product.category.products} />
+    </div>
+  );
+};
+
 export default ProductDetailPage;
