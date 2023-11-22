dafea530ada2eea40c24ae8eb587a88051f45570
Renan Franca <renanfranca.dev@gmail.com>
2023-11-22 14:48:16 -0300

    style: ensure correctly badge to deals page

--- src/app/deals/page.tsx
+++ src/app/deals/page.tsx
@@ -15,7 +15,10 @@ const DealsPage = async () => {
 
   return (
     <div className="flex flex-col gap-8 p-5">
-      <Badge>
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
         <PercentIcon size={16} />
         Ofertas
       </Badge>
