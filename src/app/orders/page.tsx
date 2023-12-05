47e56affae3d9200a9fe8721b81de8e3320ecdaf
Renan Franca <renanfranca.dev@gmail.com>
2023-12-04 22:05:19 -0300

    fix: Error Dynamic Server (build) solved

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,40 +1,37 @@
 import { Badge } from "@/components/ui/badge";
+import OrderItem from "@/components/ui/order-itens";
 import { authOptions } from "@/lib/auth";
 import { prismaClient } from "@/lib/prisma";
 import { PackageSearchIcon } from "lucide-react";
 import { getServerSession } from "next-auth";
-import OrderItem from "../../components/ui/order-itens";
+
+export const dynamic = "force-dynamic";
 
 const OrderPage = async () => {
   const user = getServerSession(authOptions);
 
-    const orders = await prismaClient.order.findMany({
-        where: {
-            userId: (user as any).id,
-        },
-        include: {
-            orderProducts: {
-                include: {
-                    product: true
-                }
-            }
-        }
-    })
-
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: (user as any).id,
+    },
+    include: {
+      orderProducts: true,
+    },
+  });
 
   return (
     <div className="p-5 lg:container lg:mx-auto lg:py-10">
       <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase mb-2"
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
       >
         <PackageSearchIcon size={16} />
         Meus Pedidos
       </Badge>
 
-    {orders.map(order => (
-        <OrderItem key={order.id} order={order}/>
-    ))}
+      {orders.map((order: any) => (
+        <OrderItem key={order.id} order={order} />
+      ))}
     </div>
   );
 };
